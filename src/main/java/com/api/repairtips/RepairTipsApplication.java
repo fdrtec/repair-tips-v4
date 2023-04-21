83585006807f2aa2ac0a664a5a3125f4571b1b1a
fdrtec <fdrtec@gmail.com>
2023-04-21 17:40:17 -0300

    [feat]:version 4
    Estrutura inicial

---
+++ src/main/java/com/api/repairtips/RepairTipsApplication.java
@@ -0,0 +1,13 @@
+package com.api.repairtips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairTipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/repairtips/RepairTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.repairtips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
