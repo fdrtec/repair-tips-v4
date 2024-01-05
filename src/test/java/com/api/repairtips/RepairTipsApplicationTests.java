9ee02737cf85f8a9d43e3423dfa20fcdca369919
fdrtec <fdrtec@gmail.com>
2024-01-05 00:39:27 -0300

    Testes com dependencias no pow

--- src/test/java/com/api/repairtips/RepairTipsApplicationTests.java
+++ src/test/java/com/api/repairtips/RepairTipsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.api.repairtips;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class RepairTipsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
